c2de458063f5a1dbb6204307477d2c165a96e8e6
Phill Conrad <pconrad@cs.ucsb.edu>
2019-10-22 12:52:30 -0700

    pc/fs WIP - one test down two to go

--- test/models/roster_student_test.rb
+++ test/models/roster_student_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class RosterStudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @course = courses(:course1)
+    @user = users(:wes)
+    @user.add_role(:admin)
+    sign_in @user
+
+    @ta_user = User.create!(
+      name: 'Chris',
+      username: 'cgaucho',
+      uid: 1234567,
+      provider: 'GitHub',
+      email: 'cgaucho@example.edu',
+      password: 'a9sd8ua98fu9as'
+    )
+    @ta_roster_student = RosterStudent.create!(
+      perm: 1234,
+      first_name: 'Chris',
+      last_name: 'Gaucho',
+      email: 'cgaucho@example.edu',
+      course: @course,
+      enrolled: false,
+      user: @ta_user
+    )
+    @ta_user.add_role :ta, @course
+
+  end
+
+  test "TA should have status TA" do
+    assert_equal "TA", @ta_roster_student.status
+  end
+
+  
+
 end
